b94bde4fe04d5e09c877b99e05eca9c0a5ea82f9
MeenYossakorn <143907645+MeenYossakorn@users.noreply.github.com>
2024-10-18 01:43:45 +0700

    FIx MouseEffect use to on navbar
    18/10/2567 - 1:43

--- src/components/Mouse/MouseEffect.js
+++ src/components/Mouse/MouseEffect.js
@@ -22,13 +22,15 @@ const MouseEffect = () => {
         className="fixed top-0 left-0 w-4 h-4 bg-white rounded-full pointer-events-none transition-transform duration-200 ease-out"
         style={{
           transform: `translate(${position.x - 8}px, ${position.y - 8}px)`,
+          zIndex: 60, // เพิ่ม z-index
         }}
       />
       {/* วงกลมใหญ่ */}
       <div
-        className=" fixed top-0 left-0 w-12 h-12 bg-transparent rounded-full border-2 border-white pointer-events-none transition-transform duration-700 ease-out"
+        className="fixed top-0 left-0 w-12 h-12 bg-transparent rounded-full border-2 border-white pointer-events-none transition-transform duration-700 ease-out"
         style={{
           transform: `translate(${position.x - 30}px, ${position.y - 30}px)`,
+          zIndex: 60, // เพิ่ม z-index
         }}
       />
     </div>
