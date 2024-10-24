9e034c44f3d7eaf85e9cc6c95b256e2148f40aae
MeenYossakorn <143907645+MeenYossakorn@users.noreply.github.com>
2024-10-25 05:22:12 +0700

    Update responsive
    25/10/2024 - 5:22

--- src/components/Content/MySkill.js
+++ src/components/Content/MySkill.js
@@ -60,11 +60,14 @@ const MySkills = () => {
             }`}
           >
             <div className="flex flex-col justify-center max-w-md">
-              <h2 className="font-roboto-mono text-3xl font-semibold text-white mb-2">
+              <h2 className="font-roboto-mono text-3xl font-semibold text-white mb-2 text-center lg:text-left">
                 MY SKILL
               </h2>
-              <p className="text-white font-roboto text-sm mb-6 pr-28">
+
+              <p className="text-white font-roboto text-sm  text-center lg:text-left">
                 These are the languages and tools I regularly use to deliver
+              </p>
+              <p className="text-white font-roboto text-sm mb-6 text-center lg:text-left">
                 efficient and modern solutions to users.
               </p>
             </div>
@@ -72,8 +75,8 @@ const MySkills = () => {
         </div>
       </div>
 
-      <div className="bg-transparent text-white  flex items-center justify-center relative ">
-        <div className="flex flex-col lg:flex-row gap-10 w-full max-w-[1220px]">
+      <div className="bg-transparent text-white flex items-center justify-center relative">
+        <div className="flex flex-col lg:flex-row gap-10 w-full max-w-[1220px] justify-center items-center">
           <div
             ref={leftSectionRef}
             className={`transition-all duration-500 ${
@@ -81,14 +84,13 @@ const MySkills = () => {
             }`}
           >
             {/* Left Section */}
-
             {/* Programming Language Section */}
-            <div className="flex flex-col lg:flex-row justify-between w-full lg:w-1/2 lg:pr-8 mb-8 lg:mb-0 ">
+            <div className="flex flex-col lg:flex-row justify-between w-full lg:w-1/2 lg:pr-8 mb-8 lg:mb-0 text-center sm:text-left">
               <div className="mb-8">
                 <h3 className="whitespace-nowrap font-roboto-mono text-lg font-semibold font-roboto text-white mb-4">
                   Programming Language
                 </h3>
-                <div className="flex space-x-6 mb-4">
+                <div className="flex justify-center sm:justify-start space-x-6 mb-4">
                   {/* HTML Icons */}
                   <div>
                     <div className="w-14 h-14 bg-gray-700 rounded-full flex items-center justify-center">
@@ -132,10 +134,10 @@ const MySkills = () => {
               </div>
             </div>
             {/* Frameworks/Libraries Section */}
-            <h3 className="font-roboto-mono text-lg font-semibold font-roboto text-white mb-4">
+            <h3 className="font-roboto-mono text-lg font-semibold font-roboto text-white mb-4 text-center sm:text-left">
               Frameworks \ Libraries
             </h3>
-            <div className="flex space-x-6 mb-4">
+            <div className="flex justify-center sm:justify-start space-x-6 mb-4">
               {/* React Icons */}
               <div className="flex flex-col items-center justify-center">
                 <div className="w-14 h-14 bg-gray-700 rounded-full flex items-center justify-center">
@@ -201,48 +203,47 @@ const MySkills = () => {
               </div>
             </div>
           </div>
-
+          {/* Right Section */}
           <div
             ref={rightSectionRef}
             className={`w-full lg:w-1/2 lg:pl-8 transition-all duration-500 ${
               isVisible.right ? "slide-in-left" : ""
-            }`}
+            } flex justify-center lg:justify-start`} // ใช้ flex และ justify-center ในจอเล็ก
           >
-            {/* Right Section */}
-            <div className="flex flex-col justify-center max-w-md">
+            <div className="flex flex-col justify-center max-w-md text-center sm:text-left">
               <div className="mb-8">
-                <h3 className="font-roboto-mono text-lg font-semibold font-roboto text-white mb-4">
+                <h3 className="font-roboto-mono text-lg font-semibold text-white mb-4">
                   Tools
                 </h3>
-                <div className="flex space-x-6 mb-4">
+                <div className="flex justify-center sm:justify-start space-x-6 mb-4">
+                  {" "}
+                  {/* flex justify-center */}
                   {/* Figma Icons */}
                   <div className="flex flex-col items-center justify-center">
                     <div className="w-14 h-14 bg-gray-700 rounded-full flex items-center justify-center">
                       <img
                         src="https://s3-alpha.figma.com/hub/file/1481185752/fa4cd070-6a79-4e1b-b079-8b9b76408595-cover.png"
                         alt="Figma Icon"
-                        className="w-6 h-8"
+                        className="w-8 h-8"
                       />
                     </div>
                     <p className="font-roboto text-white text-center mt-1">
                       Figma
                     </p>
                   </div>
-
                   {/* Canva Icons */}
                   <div className="flex flex-col items-center justify-center">
                     <div className="w-14 h-14 bg-gray-700 rounded-full flex items-center justify-center">
                       <img
                         src="https://img.icons8.com/fluent/512/canva.png"
                         alt="Canva Icon"
-                        className="w-12 h-12"
+                        className="w-10 h-10"
                       />
                     </div>
                     <p className="font-roboto text-white text-center mt-1">
                       Canva
                     </p>
                   </div>
-
                   {/* Photoshop Icons */}
                   <div className="flex flex-col items-center justify-center">
                     <div className="w-14 h-14 bg-gray-700 rounded-full flex items-center justify-center">
@@ -258,13 +259,16 @@ const MySkills = () => {
                   </div>
                 </div>
               </div>
-              <h3 className="whitespace-nowrap font-roboto-mono text-lg font-semibold font-roboto text-white mb-4">
+
+              <h3 className="whitespace-nowrap font-roboto-mono text-lg font-semibold text-white mb-4">
                 Database \ Version Control
               </h3>
-              <div className="flex space-x-6 mb-4">
+              <div className="flex justify-center sm:justify-start space-x-6 mb-4">
+                {" "}
+                {/* flex justify-center */}
                 {/* MySQL Icons */}
-                <div>
-                  <div className="w-14 h-14 bg-gray-700 rounded-full flex flex-col items-center justify-center">
+                <div className="flex flex-col items-center justify-center">
+                  <div className="w-14 h-14 bg-gray-700 rounded-full flex items-center justify-center">
                     <img
                       src="https://cdn-icons-png.flaticon.com/512/5968/5968313.png"
                       alt="MySQL Icon"
@@ -276,12 +280,12 @@ const MySkills = () => {
                   </p>
                 </div>
                 {/* Firebase Icons */}
-                <div>
-                  <div className="w-14 h-14 bg-gray-700 rounded-full flex flex-col items-center justify-center">
+                <div className="flex flex-col items-center justify-center">
+                  <div className="w-14 h-14 bg-gray-700 rounded-full flex items-center justify-center">
                     <img
                       src="https://cdn4.iconfinder.com/data/icons/google-i-o-2016/512/google_firebase-2-512.png"
                       alt="Firebase Icon"
-                      className="w-12 h-12"
+                      className="w-10 h-10"
                     />
                   </div>
                   <p className="font-roboto text-white text-center mt-1">
@@ -289,12 +293,12 @@ const MySkills = () => {
                   </p>
                 </div>
                 {/* Git Icons */}
-                <div>
+                <div className="flex flex-col items-center justify-center">
                   <div className="w-14 h-14 bg-gray-700 rounded-full flex items-center justify-center">
                     <img
                       src="https://seeklogo.com/images/G/git-logo-CD8D6F1C09-seeklogo.com.png"
-                      alt="Firebase Icon"
-                      className="w-10 h-10"
+                      alt="Git Icon"
+                      className="w-8 h-8"
                     />
                   </div>
                   <p className="font-roboto text-white text-center mt-1">Git</p>
